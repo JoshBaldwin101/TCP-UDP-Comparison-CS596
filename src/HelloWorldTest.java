2fb6639057f40b9bff0b3d4aaf1c0c6df5466169
Josh Baldwin <jmbaldwin@sdsu.edu>
2020-10-14 21:12:57 -0700

    Testing new Josh changes.

--- src/HelloWorldTest.java
+++ src/HelloWorldTest.java
@@ -3,6 +3,6 @@ public class HelloWorldTest {
 	public static void main(String[] args)
 	{
 		// This is a test for the repository to make sure we can all download the project hi josh
-		System.out.println("Hello World! If you're seeing this code then you've successfully downloaded our repository! -Josh");
+		System.out.println("Hello World! If you're seeing this code then you've successfully downloaded our repository! Testtesttesttesttest! -Josh");
 	}
 }
