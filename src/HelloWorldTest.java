7eb6674765527eee0bed2d67c616029c87d8119d
Matt Magoon <mmagoon@sdsu.edu>
2020-10-14 21:09:44 -0700

    Matt's changes

--- src/HelloWorldTest.java
+++ src/HelloWorldTest.java
@@ -2,7 +2,7 @@
 public class HelloWorldTest {
 	public static void main(String[] args)
 	{
-		// This is a test for the repository to make sure we can all download the project
+		// This is a test for the repository to make sure we can all download the project hi josh
 		System.out.println("Hello World! If you're seeing this code then you've successfully downloaded our repository! -Josh");
 	}
 }
