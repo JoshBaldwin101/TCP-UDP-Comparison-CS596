63030170d358a54b6fb79410d0c09c84d12e11ab
Josh Baldwin <jmbaldwin@sdsu.edu>
2020-10-14 20:25:12 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# TCP-UDP-Comparison-CS596
+For our CS596 Wireless Networks group project. This repository hosts our code for our project designed to simulate TCP and UDP.
---
+++ src/HelloWorldTest.java
@@ -0,0 +1,8 @@
+
+public class HelloWorldTest {
+	public static void main(String[] args)
+	{
+		// This is a test for the repository to make sure we can all download the project
+		System.out.println("Hello World! If you're seeing this code then you've successfully downloaded our repository! -Josh");
+	}
+}
